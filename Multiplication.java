b140c262a2a386821ec3f1bbbd5da3d0db1d672d
Khare-5 <timepassdk5@gmail.com>
2025-01-07 06:56:48 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,7 @@
+package Calculator;
+
+public class Multiplication {
+    public static int mul(int num1, int num2) {
+        return num1*num2;
+    }
+}
---
+++ Substraction.java
@@ -0,0 +1,7 @@
+package Calculator;
+
+public class Substraction {
+    public static int sub(int num1, int num2) {
+        return num1 - num2;
+    }
+}

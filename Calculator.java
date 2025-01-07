d4d7474ffe783cc88cde1f8b6bd6e88d3b3dd52f
Khare-5 <timepassdk5@gmail.com>
2025-01-07 06:55:45 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+package Calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println(Addition.add(1,2));
+        System.out.println(Substraction.sub(1,2));
+        System.out.println(Multiplication.mul(1,2));
+        System.out.println(Division.div(1,2));
+    }
+}
